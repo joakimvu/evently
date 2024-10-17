8131a29a91fb06b83ba150ed625d26c656df8275
Joakim Vu <joakimvu@icloud.com>
2024-10-17 16:43:47 +0200

    Update route.ts

--- app/api/webhooks/clerk/route.ts
+++ app/api/webhooks/clerk/route.ts
@@ -66,8 +66,8 @@ export async function POST(req: Request) {
       clerkId: id,
       email: email_addresses[0].email_address,
       username: username!,
-      firstName: first_name || "",
-      lastName: last_name || "",
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
@@ -80,7 +80,6 @@ export async function POST(req: Request) {
         },
       });
     }
-
     return NextResponse.json({ message: "OK", user: newUser });
   }
 
@@ -90,8 +89,8 @@ export async function POST(req: Request) {
 
     const user = {
       username: username!,
-      firstName: first_name || "",
-      lastName: last_name || "",
+      firstName: first_name!,
+      lastName: last_name!,
       photo: image_url,
     };
 
@@ -108,6 +107,5 @@ export async function POST(req: Request) {
 
     return NextResponse.json({ message: "OK", user: deletedUser });
   }
-
   return new Response("", { status: 200 });
 }
