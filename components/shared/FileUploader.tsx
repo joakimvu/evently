38a20628d0fe31172aa4dd4de2227714af993dd3
Joakim Vu <joakimvu@icloud.com>
2024-10-18 12:05:00 +0200

    Update FileUploader.tsx

--- components/shared/FileUploader.tsx
+++ components/shared/FileUploader.tsx
@@ -25,7 +25,7 @@ export function FileUploader({
 
   const { getRootProps, getInputProps } = useDropzone({
     onDrop,
-    accept: "image/*" ? generateClientDropzoneAccept(["image/*"]) : undefined,
+    accept: generateClientDropzoneAccept(["image/*"]),
   });
 
   return (
